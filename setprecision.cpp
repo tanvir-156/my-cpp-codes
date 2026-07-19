045fcb461c8e113335e924f229ab7d6c60310de8
Md Tanvir Hasan Chowdhury <iamtanvir2003@gmail.com>
2026-07-19 20:53:09 +0600

    Add files via upload

---
+++ setprecision.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double d= 23.45676;
+    cout<< fixed<< setprecision(2)<<d<<endl;
+    return 0;
+}
---
+++ ternaryoperator.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x=6;
+    x%2==0 ? cout<< "Even\n" : cout<<"Odd";
+}
